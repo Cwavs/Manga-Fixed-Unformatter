45acbd91ed88e2d60fa7dc0c983869470d85dd24
RivenSkaye <pretender0906@gmail.com>
2021-08-05 23:34:57 +0200

    Fixed some stuff to make it run smoother. Slightly slower because I broke '*.png', but allows for more image formats now

--- Manga Fixed Unformatter.py
+++ Manga Fixed Unformatter.py
@@ -1,18 +1,29 @@
 import os
+from pathlib import Path
 import subprocess
-import shutil
 
 print("Manga Fixed Unformatter V1.0")
-print("Written by Cwavs, with lots of pain (fuck you image magick docs)")
-try:
-    os.mkdir("out")
-except:
-    shutil.rmtree("out")
-    os.mkdir("out")
+print("Written by Cwavs, with lots of pain (fuck you image magick docs).\
+       Small fixes made by Riven, with lots of love.")
+# Account for users being idiots
+print(f"Currently looking in {Path.cwd()}. Is this correct?")
+target = input("Provide path or press enter > ") or Path.cwd()
+# Make sure the path is absolute
+target = Path(str(target)).resolve()
+if not target.exists():
+    print("Provided directory does not exist. Try again.")
+    # Exit and notify the system there was an error.
+    # This is good for automation purposes
+    exit(1)
+out_path = Path(str(target)+"/out").resolve()
+# Make sure the out path exists
+out_path.mkdir(parents=True, exist_ok=True)
+files = sorted(target.glob("*.png"))
+# Grab the cover page width and height
+width, height = subprocess.run(["magick", "identify", "-format", "%w;%h", files[0]], stdout=subprocess.PIPE).stdout.decode('utf-8').split(";")
+
 print("Splitting Pages...")
-subprocess.call("magick mogrify -path out -crop 50%x100% -reverse -quality 100 *.png", shell=True)
-shutil.copy("page000.png", "out")
-os.system("cd out")
-os.remove("page000-0.png")
-os.remove("page000-1.png")
+# Set those as the max sizes for every page, this also "crops" the cover to its own sizes
+for f in files:
+    subprocess.run(["magick", "mogrify", "-path", str(out_path), "-crop", f"{width}x{height}!", "-reverse", "-quality", "100", f])
 print("Done!")
